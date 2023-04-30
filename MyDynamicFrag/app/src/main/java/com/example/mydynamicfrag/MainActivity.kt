e7b76e0a73b709d4bbb7dabb2c36fd6735626c88
jg-konkuk <anjeong8577@naver.com>
2023-04-30 20:49:19 +0900

    1

--- MyDynamicFrag/app/src/main/java/com/example/mydynamicfrag/MainActivity.kt
+++ MyDynamicFrag/app/src/main/java/com/example/mydynamicfrag/MainActivity.kt
@@ -6,7 +6,8 @@ import com.example.mydynamicfrag.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
-
+    val imageFrag=ImageFragment()
+    val itemFrag=ItemFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -15,6 +16,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initLayout(){
-
+        val fragment = supportFragmentManager.beginTransaction()
+        fragment.replace(R.id.frameLayout, imageFrag)
+        fragment.commit()
+        binding.apply {
+            button.setOnClickListener{
+                if(!imageFrag.isVisible){
+                    val fragment = supportFragmentManager.beginTransaction()
+                    fragment.addToBackStack(null)
+                    fragment.replace(R.id.frameLayout, imageFrag)
+                    fragment.commit()
+                }
+            }
+            button2.setOnClickListener{
+                if(!itemFrag.isVisible){
+                    val fragment = supportFragmentManager.beginTransaction()
+                    fragment.addToBackStack(null)
+                    fragment.replace(R.id.frameLayout, itemFrag)
+                    fragment.commit()
+                }
+            }
+        }
     }
 }
