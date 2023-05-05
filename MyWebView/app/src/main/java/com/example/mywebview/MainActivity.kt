633772d39a5c2e20e42aeffea7e73087c6ad6a57
jg-konkuk <anjeong8577@naver.com>
2023-05-05 22:09:41 +0900

    1

--- MyWebView/app/src/main/java/com/example/mywebview/MainActivity.kt
+++ MyWebView/app/src/main/java/com/example/mywebview/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.mywebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import com.example.mywebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init(){
+        binding.apply {
+            webView.webViewClient= WebViewClient()
+            webView.settings.javaScriptEnabled=true
+            webView.settings.builtInZoomControls=true
+            webView.settings.defaultTextEncodingName="utf-8"
+            webView.loadUrl("https://www.google.com")
+        }
     }
 }
