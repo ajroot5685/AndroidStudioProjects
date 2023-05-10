19328a78e9166ca88b857293c960034ecd9b157a
jg-konkuk <anjeong8577@naver.com>
2023-05-10 22:02:00 +0900

    1

--- MyAppChat/app/src/main/java/com/example/myappchat/MainActivity.kt
+++ MyAppChat/app/src/main/java/com/example/myappchat/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.myappchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.myappchat.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    var url="https://api.openai.com/v1/completions"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.apply {
+            sendBtn.setOnClickListener {
+                responseTV.text="잠시 기다려주세요..."
+                if(userQuestion.text.toString().length>0){
+                    getResponse(userQuestion.text.toString())
+                }else{
+                    Toast.makeText(this@MainActivity,"질문을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun getResponse(query: String) {
+        binding.questionTV.text=query
+        binding.userQuestion.setText("")
+
+        val queue = Volley.newRequestQueue(this)
+        val jsonObject=JSONObject()
+        jsonObject.put("model","text-davinci-003")
+        jsonObject.put("prompt",query)
+        jsonObject.put("temperature",0)
+        jsonObject.put("max_tokens",200)
+
+        val postRequest = object: JsonObjectRequest(Method.POST, url, jsonObject,
+            Response.Listener {
+//                Log.i("check", it.toString())
+                val msg = it.getJSONArray("choices").getJSONObject(0).getString("text")
+                binding.responseTV.text=msg
+            },
+            Response.ErrorListener {
+                Log.e("Error", it.message.toString())
+            }){
+            override fun getHeaders(): MutableMap<String, String> {
+                val header = HashMap<String, String>()
+                header["Content-Type"]="application/json"
+//                header["Authorization"]="Bearer your-api-key"
+                header["Authorization"]="Bearer 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"
+                return header
+            }
+        }
+        queue.add(postRequest)
     }
 }
--- MyGetNews/app/src/main/java/com/example/mygetnews/MainActivity.kt
+++ MyGetNews/app/src/main/java/com/example/mygetnews/MainActivity.kt
@@ -14,11 +14,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
+import org.jsoup.parser.Parser
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var adapter: MyAdapter
     val url="https://news.daum.net"
+    val rssurl="https://fs.jtbc.co.kr/RSS/culture.xml"
+    val musicurl="https://www.melon.com/chart/index.htm"
     val scope= CoroutineScope(Dispatchers.IO)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +33,7 @@ class MainActivity : AppCompatActivity() {
     fun getnews(){
         scope.launch {
             adapter.items.clear()
+            // get() 의 default는 html parser
             val doc = Jsoup.connect(url).get()
 //            Log.i("check", doc.toString())
             val headlines = doc.select("ul.list_newsissue>li>div.item_issue>div>strong.tit_g>a")
@@ -43,10 +47,47 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun getrssnews(){
+        scope.launch {
+            adapter.items.clear()
+            val doc = Jsoup.connect(rssurl).parser(Parser.xmlParser()).get()
+//            Log.i("check", doc.toString())
+            val headlines = doc.select("item")
+            for(news in headlines){
+                adapter.items.add(MyData(news.select("title").text(), news.select("link").text()))
+            }
+            withContext(Dispatchers.Main){
+                adapter.notifyDataSetChanged()
+                binding.swipe.isRefreshing = false
+            }
+        }
+    }
+
+    fun getmusics(){
+        scope.launch {
+            adapter.items.clear()
+            // get() 의 default는 html parser
+            val doc = Jsoup.connect(musicurl).get()
+//            Log.i("check", doc.toString())
+            val headlines = doc.select("form#frm>div>table>tbody>tr.lst50")
+            for(news in headlines){
+//                adapter.items.add(MyData(news.text(),""))
+                adapter.items.add(MyData(news.select("div.wrap_song_info>div.ellipsis.rank01").text(),""))
+                adapter.items.add(MyData(news.select("td>div.wrap>div.wrap_song_info>div.ellipsis.rank02>a").text(), ""))
+            }
+            withContext(Dispatchers.Main){
+                adapter.notifyDataSetChanged()
+                binding.swipe.isRefreshing = false
+            }
+        }
+    }
+
     private fun initLayout(){
         binding.swipe.setOnRefreshListener {
             binding.swipe.isRefreshing = true
-            getnews()
+//            getnews()
+//            getrssnews()
+            getmusics()
         }
         binding.recyclerView.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         binding.recyclerView.addItemDecoration(DividerItemDecoration(this,LinearLayoutManager.VERTICAL))
@@ -58,6 +99,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
         binding.recyclerView.adapter=adapter
-        getnews()
+//        getnews()
+//        getrssnews()
+        getmusics()
     }
 }
