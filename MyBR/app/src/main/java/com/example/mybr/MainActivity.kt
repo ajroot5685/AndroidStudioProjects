c84a238909dd56c241e0760ebe9d75b2486b6555
jg-konkuk <anjeong8577@naver.com>
2023-05-22 00:24:07 +0900

    1

--- MyBR/app/src/main/java/com/example/mybr/MainActivity.kt
+++ MyBR/app/src/main/java/com/example/mybr/MainActivity.kt
@@ -4,9 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.registerReceiver
 import com.example.mybr.databinding.ActivityMainBinding
 
@@ -18,7 +23,81 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initPermission()
         initLayout()
+        //getMessage(intent)
+        checkSettingOverlayWindow(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        checkSettingOverlayWindow(intent)
+    }
+
+    fun getMessage(intent: Intent?) {
+        if(intent!=null){
+            if (intent.hasExtra("msgSender") and intent.hasExtra("msgBody")){
+                val msgSender = intent.getStringExtra("msgSender")
+                val msgBody = intent.getStringExtra("msgBody")
+                Toast.makeText(this, "보낸번호 :"+msgSender+"\n"+msgBody, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun checkSettingOverlayWindow(intent: Intent?) {
+        if(Settings.canDrawOverlays(this)){
+            getMessage(intent)
+        }else{
+            val overlayIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            requestSettingLauncher.launch(overlayIntent)
+        }
+    }
+
+    val requestSettingLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(Settings.canDrawOverlays(this)){
+            getMessage(this.intent)
+        }else{
+            Toast.makeText(this, "권한 승인이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        if(it){
+            initPermission()
+        }else{
+            Toast.makeText(this, "권한 승인이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun permissionAlertDlg(){
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("반드시 문자 수신 권한이 허용되어야 합니다.")
+            .setTitle("권한 체크")
+            .setPositiveButton("OK"){
+                _,_->
+                requestPermissionLauncher.launch(android.Manifest.permission.RECEIVE_SMS)
+            }
+            .setNegativeButton("Cancel"){
+                dlg,_->dlg.dismiss()
+            }
+        val dlg = builder.create()
+        dlg.show()
+    }
+
+    private fun initPermission() {
+        when{
+            (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED)-> {
+                Toast.makeText(this, "문자 수신 동의함", Toast.LENGTH_SHORT).show()
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.RECEIVE_SMS)->{
+                permissionAlertDlg()
+            }
+
+            else ->{
+                requestPermissionLauncher.launch(android.Manifest.permission.RECEIVE_SMS)
+            }
+        }
     }
 
     private fun initLayout() {
---
+++ MyBR/app/src/main/java/com/example/mybr/MyReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.mybr
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import android.util.Log
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if(intent.action.equals("android.provider.Telephony.SMS_RECEIVED")){
+            val msg = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+//            for(smsMessage in msg){
+//                Log.i("msg", smsMessage.displayMessageBody)
+//            }
+            val newIntent = Intent(context, MainActivity::class.java)
+            newIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            newIntent.putExtra("msgSender", msg[0].originatingAddress)
+            newIntent.putExtra("msgBody", msg[0].messageBody)
+            context.startActivity(newIntent)
+        }
+    }
+}
