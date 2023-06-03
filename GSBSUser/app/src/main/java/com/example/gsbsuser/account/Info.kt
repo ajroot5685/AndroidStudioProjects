3d8e97810adf91949fdcb2b7cfc155554aab4aad
ajroot5685 <anjeong8577@naver.com>
2023-06-03 16:31:14 +0900

    Feat: GBSB Info update

--- GSBSUser/app/src/main/java/com/example/gsbsuser/account/Info.kt
+++ GSBSUser/app/src/main/java/com/example/gsbsuser/account/Info.kt
@@ -1,3 +1,11 @@
 package com.example.gsbsuser.account
 
-data class Info(var email:String="", var call:String="", var birth:String="", var major:String="", var introduce:String="")
+data class Info(
+    var email:String,
+    var call:String,
+    var birth:String,
+    var major:String,
+    var introduce:String
+    ){
+    constructor(): this("","","","","")
+}
